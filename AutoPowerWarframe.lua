1d4330ec228a8a907d21140d7d05c50133c59e52
4n3ver <4n3ver@reborn.com>
2022-06-08 01:00:19 -0400

    refactor

--- AutoPowerWarframe.lua
+++ AutoPowerWarframe.lua
@@ -1,4 +1,8 @@
-Event = {
+Family = {
+    MOUSE = "mouse"
+}
+
+MouseEvent = {
     MOUSE_BUTTON_PRESSED = "MOUSE_BUTTON_PRESSED",
     MOUSE_BUTTON_RELEASED = "MOUSE_BUTTON_RELEASED"
 }
@@ -44,67 +48,89 @@ AutoPower = {
         if self._toggled then
             AbortMacro()
             self._toggled = false
+            OutputLogMessage("Stopped Firing " .. self._selected .. "\n")
         else
             self._toggled = true
             PlayMacro("Toggle Press " .. self._selected)
+            OutputLogMessage("Start Firing " .. self._selected .. "\n")
         end
     end,
 
     _selectPrev = function(self)
-        if self._selected > self.MIN and not self._toggled then
+        if not self._toggled and self._selected > self.MIN then
             self._selected = self._selected - 1
+            OutputLogMessage("Selecting " .. self._selected .. "\n")
         end
     end,
 
     _selectNext = function(self)
-        if self._selected < self.MAX and not self._toggled then
+        if not self._toggled and self._selected < self.MAX then
             self._selected = self._selected + 1
+            OutputLogMessage("Selecting " .. self._selected .. "\n")
         end
     end,
 
-    OnEvent = function(self, event, arg, family)
-        if event == Event.MOUSE_BUTTON_PRESSED then
-            if arg == Configuration.FIRE and not self._firing then
-                self._firing = true
-                if self._firing then
-                    PressMouseButton(MouseButton.LEFT)
-                    OutputLogMessage("FIRE Pressed\n")
-                end
-            elseif arg == Configuration.AIM and not self._aiming then
-                self._aiming = true
-                if self._aiming then
-                    PressMouseButton(MouseButton.RIGHT)
-                    OutputLogMessage("AIM Pressed\n")
-                end
+    _holdFire = function(self)
+        self._firing = true
+        if self._firing then
+            PressMouseButton(MouseButton.LEFT)
+            OutputLogMessage("FIRE Pressed\n")
+        end
+    end,
+
+    _holdAim = function(self)
+        self._aiming = true
+        if self._aiming then
+            PressMouseButton(MouseButton.RIGHT)
+            OutputLogMessage("AIM Pressed\n")
+        end
+    end,
+
+    _releaseFire = function(self)
+        self._firing = false
+        if not self._firing then
+            ReleaseMouseButton(MouseButton.LEFT)
+            OutputLogMessage("FIRE Released\n")
+        end
+    end,
+
+    _releaseAim = function(self)
+        self._aiming = false
+        if not self._aiming then
+            ReleaseMouseButton(MouseButton.RIGHT)
+            OutputLogMessage("AIM Released\n")
+        end
+    end,
+
+    OnMouseEvent = function(self, event, arg)
+        if event == MouseEvent.MOUSE_BUTTON_PRESSED then
+            if not self._firing and arg == Configuration.FIRE then
+                self:_holdFire()
+            elseif not self._aiming and arg == Configuration.AIM then
+                self:_holdAim()
+            elseif not self._aiming and arg == Configuration.TOGGLE then
+                self:_togglePressSelected()
             elseif arg == Configuration.PREV then
                 self:_selectPrev()
             elseif arg == Configuration.NEXT then
                 self:_selectNext()
-            elseif arg == Configuration.TOGGLE and not self._aiming then
-                self:_togglePressSelected()
             end
-        elseif event == Event.MOUSE_BUTTON_RELEASED then
-            if arg == Configuration.FIRE and self._firing then
-                self._firing = false
-                if not self._firing then
-                    ReleaseMouseButton(MouseButton.LEFT)
-                    OutputLogMessage("FIRE Released\n")
-                end
-            elseif arg == Configuration.AIM and self._aiming then
-                self._aiming = false
-                if not self._aiming then
-                    ReleaseMouseButton(MouseButton.RIGHT)
-                    OutputLogMessage("AIM Released\n")
-                end
+        elseif event == MouseEvent.MOUSE_BUTTON_RELEASED then
+            if self._firing and arg == Configuration.FIRE then
+                self:_releaseFire()
+            elseif self._aiming and arg == Configuration.AIM then
+                self:_releaseAim()
             end
         end
-    end,
+    end
 }
 
 function OnEvent(event, arg, family)
     OutputLogMessage("event = %s, arg = %s, family = %s\n", event, arg, family)
-    EnablePrimaryMouseButtonEvents(true)
-    AutoPower:OnEvent(event, arg, family)
+    if family == Family.MOUSE then
+        AutoPower:OnMouseEvent(event, arg)
+    end
 end
 
+EnablePrimaryMouseButtonEvents(true)
 OutputLogMessage("Starting AutoPower\n")
