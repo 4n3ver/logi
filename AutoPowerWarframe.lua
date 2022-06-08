132b20610e7817213746fa17e7fab2d2040f0cea
4n3ver <4n3ver@reborn.com>
2022-06-08 16:33:21 -0400

    filter out rapid click event

--- AutoPowerWarframe.lua
+++ AutoPowerWarframe.lua
@@ -32,105 +32,114 @@ Configuration = {
     FIRE = MouseButtonEvent.LEFT,
     AIM = MouseButtonEvent.RIGHT,
     PREV = MouseButtonEvent.SCROLL_LEFT,
-    NEXT = MouseButtonEvent.SCROLL_RIGHT
+    NEXT = MouseButtonEvent.SCROLL_RIGHT,
+    DEBOUNCE_DELAY_MS = 30,
 }
+
+DebouncedButton = {
+    RELEASED = MouseEvent.MOUSE_BUTTON_RELEASED,
+    PRESSED = MouseEvent.MOUSE_BUTTON_PRESSED,
+
+    debounce_delay = Configuration.DEBOUNCE_DELAY_MS,
+    button = -1,
+
+    state = MouseEvent.MOUSE_BUTTON_RELEASED,
+    lastDebounceTime = GetRunningTime(),
+}
+
+function DebouncedButton:new(obj)
+    obj = obj or {}
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+function DebouncedButton:isPressed()
+    return self.state == self.PRESSED
+end
+
+function DebouncedButton:switch()
+    if self:isPressed() then
+        PressMouseButton(self.button)
+        OutputLogMessage("\tButton %s pressed\n", self.button)
+    else
+        ReleaseMouseButton(self.button)
+        OutputLogMessage("\tButton %s released\n", self.button)
+    end
+end
+
+function DebouncedButton:onEvent(new_state)
+    local sinceLastDebounce = GetRunningTime() - self.lastDebounceTime
+    self.lastDebounceTime = GetRunningTime()
+    if sinceLastDebounce > self.debounce_delay then
+        OutputLogMessage("Since last debounce of button %s: %sms\n", self.button, sinceLastDebounce)
+        self.state = new_state
+        self:switch()
+    else
+        OutputLogMessage("\tButton %s debounced\n", self.button)
+    end
+end
 
 AutoPower = {
     MAX = 4,
     MIN = 1,
 
-    _selected = 1,
-    _toggled = false,
-    _aiming = false,
-    _firing = false,
+    selected = 1,
+    isActive = false,
 
-    _togglePressSelected = function(self)
-        if self._toggled then
+    activate = function(self)
+        if self.isActive then
             AbortMacro()
-            self._toggled = false
-            OutputLogMessage("Stopped Firing " .. self._selected .. "\n")
+            self.isActive = false
+            OutputLogMessage("\tStopped Firing %s\n", self.selected)
         else
-            self._toggled = true
-            PlayMacro("Toggle Press " .. self._selected)
-            OutputLogMessage("Start Firing " .. self._selected .. "\n")
+            self.isActive = true
+            PlayMacro("Toggle Press " .. self.selected)
+            OutputLogMessage("\tStart Firing %s\n", self.selected)
         end
     end,
 
-    _selectPrev = function(self)
-        if not self._toggled and self._selected > self.MIN then
-            self._selected = self._selected - 1
-            OutputLogMessage("Selecting " .. self._selected .. "\n")
+    previous = function(self)
+        if not self.isActive and self.selected > self.MIN then
+            self.selected = self.selected - 1
+            OutputLogMessage("\tSelecting %s\n", self.selected)
         end
     end,
 
-    _selectNext = function(self)
-        if not self._toggled and self._selected < self.MAX then
-            self._selected = self._selected + 1
-            OutputLogMessage("Selecting " .. self._selected .. "\n")
+    next = function(self)
+        if not self.isActive and self.selected < self.MAX then
+            self.selected = self.selected + 1
+            OutputLogMessage("\tSelecting %s\n", self.selected)
         end
-    end,
+    end
+}
 
-    _holdFire = function(self)
-        self._firing = true
-        if self._firing then
-            PressMouseButton(MouseButton.LEFT)
-            OutputLogMessage("FIRE Pressed\n")
-        end
-    end,
+Mouse = {
+    leftButton = DebouncedButton:new{button = MouseButton.LEFT},
+    rightButton = DebouncedButton:new{button = MouseButton.RIGHT, debounce_delay = 75},
 
-    _holdAim = function(self)
-        self._aiming = true
-        if self._aiming then
-            PressMouseButton(MouseButton.RIGHT)
-            OutputLogMessage("AIM Pressed\n")
-        end
-    end,
-
-    _releaseFire = function(self)
-        self._firing = false
-        if not self._firing then
-            ReleaseMouseButton(MouseButton.LEFT)
-            OutputLogMessage("FIRE Released\n")
-        end
-    end,
-
-    _releaseAim = function(self)
-        self._aiming = false
-        if not self._aiming then
-            ReleaseMouseButton(MouseButton.RIGHT)
-            OutputLogMessage("AIM Released\n")
-        end
-    end,
-
-    OnMouseEvent = function(self, event, arg)
-        if event == MouseEvent.MOUSE_BUTTON_PRESSED then
-            if not self._firing and arg == Configuration.FIRE then
-                self:_holdFire()
-            elseif not self._aiming and arg == Configuration.AIM then
-                self:_holdAim()
-            elseif not self._aiming and arg == Configuration.TOGGLE then
-                self:_togglePressSelected()
+    onEvent = function(self, event, arg)
+        if arg == Configuration.FIRE then
+            self.leftButton:onEvent(event)
+        elseif arg == Configuration.AIM then
+            self.rightButton:onEvent(event)
+        elseif event == MouseEvent.MOUSE_BUTTON_PRESSED then
+            if arg == Configuration.TOGGLE and not self.rightButton:isPressed() then
+                AutoPower:activate()
             elseif arg == Configuration.PREV then
-                self:_selectPrev()
+                AutoPower:previous()
             elseif arg == Configuration.NEXT then
-                self:_selectNext()
-            end
-        elseif event == MouseEvent.MOUSE_BUTTON_RELEASED then
-            if self._firing and arg == Configuration.FIRE then
-                self:_releaseFire()
-            elseif self._aiming and arg == Configuration.AIM then
-                self:_releaseAim()
+                AutoPower:next()
             end
         end
     end
 }
 
 function OnEvent(event, arg, family)
-    OutputLogMessage("event = %s, arg = %s, family = %s\n", event, arg, family)
+    OutputLogMessage("runtime = %sms, event = %s, arg = %s, family = %s\n", GetRunningTime(), event, arg, family)
     if family == Family.MOUSE then
-        AutoPower:OnMouseEvent(event, arg)
+        Mouse:onEvent(event, arg)
     end
 end
-
 EnablePrimaryMouseButtonEvents(true)
 OutputLogMessage("Starting AutoPower\n")
